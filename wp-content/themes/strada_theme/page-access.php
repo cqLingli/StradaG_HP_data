e431a08483b84b7c0f6d6b81f5879b957560e21a
lynlyntan <lyntan@outlook.jp>
2023-07-24 16:18:38 +0900

    access sz

--- wp-content/themes/strada_theme/page-access.php
+++ wp-content/themes/strada_theme/page-access.php
@@ -1,4 +1,4 @@
-<?php get_header(); ?>
+<?php get_header("sz"); ?>
 
 <main class="l-main p-access">
     <div class="c-breakcrumds">
@@ -18,9 +18,56 @@
       </div>
     </div>
 
-    <div class="l-container">
+    <div class="l-container accessbox">
+        <div class="access-title">アクセス</div>
         <div class="p-about_groupContent">
-            <?php get_template_part( 'template', 'access' ); ?>
+            <div class="t-access t-block" id="access">
+                <div class="l-container">
+                    <div class="t-access_main">
+                        <div class="t-access_img">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/top/img_access.jpg" alt="">
+                        </div>
+                        <div class="t-access_content">
+                            <?php if(!is_page('access')) : ?>
+                            <div class="c-tlt01 c-tlt01__black">
+                                <h2 class="c-tlt01__line">
+                                    アクセス
+                                    <span>ACCSESS</span>
+                                </h2>
+                            </div>
+                            <?php endif; ?>
+                                <h4>ストラーダ税理士法人</h4>
+                                <p class="t-access_add">
+                                東京都中央区日本橋蛎殻町2丁目11−2 オートエックス工藤ビル4F
+                                </p>
+                                <p class="t-access_add">
+                                TEL:0120-709-459
+                                </p>
+                            <ul>
+                                <li>
+                                    <span>半蔵門線：水天宮前駅 5番出口から徒歩2分</span>
+                                </li>
+                                <li>
+                                    <span>日比谷線：人形町駅 A2出口から徒歩5分 </span>
+                                </li>
+                                <li>
+                                    <span>浅草線：人形町駅 A2出口から徒歩5分</span>
+                                </li>
+                                <li>
+                                    <span>都営新宿線：浜町駅 A2出口から徒歩9分</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <?php if(!is_page('access')) : ?>
+                        <div class="t-access_map">
+                            <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.732793825305!2d139.78441761525877!3d35.68358088019382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188945bc013343%3A0x91fe51cc7df02e4d!2z5pel5pys44CB44CSMTAzLTAwMTQg5p2x5Lqs6YO95Lit5aSu5Yy65pel5pys5qmL6JuO5q6755S677yS5LiB55uu77yR77yR4oiS77ySIOOCquODvOODiOOCqOODg-OCr-OCueW3peiXpOODk-ODqyA0Rg!5e0!3m2!1sja!2s!4v1619871108536!5m2!1sja!2s"
+                            width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        </div>
+                        <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
             <div class="access-list">
                 <div class="access" id="access01">
                     <h3 class="cm-ttl01">半蔵門線「水天宮前駅」からお越しの場合</h3>
