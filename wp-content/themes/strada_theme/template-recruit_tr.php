55f0566b4e0e497130be811532525e5e8833f31b
lynlyntan <lyntan@outlook.jp>
2023-07-26 17:23:16 +0900

    recruit add

---
+++ wp-content/themes/strada_theme/template-recruit_tr.php
@@ -0,0 +1,80 @@
+<main class="l-main p-recruit">
+  <div class="c-breakcrumds">
+    <div class="l-container">
+      <span property="itemListElement" typeof="ListItem"><a property="item" typeof="WebPage" title="Go to 中央区日本橋の税理士法人【ストラーダ税理士法人】." href="https://strada-tax.jp" class="home" ><span property="name">TOP</span></a><meta property="position" content="1"></span>/<span property="itemListElement" typeof="ListItem"><span property="name" class="post post-page current-item">採用情報</span><meta property="url" content="https://strada-tax.jp/recruit/"><meta property="position" content="2"></span>    </div>
+    </div>
+  
+    <div class="c-mainTitle">
+      <div class="c-mainTitle_content">
+        <div class="c-tlt01 c-tlt01__black">
+          <h1 class="c-tlt01__line c-tlt01__line--gray">
+            採用情報
+            <span>RECRUIT</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+
+    <div class="company_infomation">
+      <p>
+      タックスリアルティタックスリアルティタックスリアルティタックスリアルティタックスリアルティタックスリアルティタックスリアルティ
+      </p>
+    </div>  
+
+    <div class="t-recruit">
+      <div class="recruit-container">                           
+        <div class="t-recruitContent" id="募集要項" >
+          <div class="t-recruitHead">                      
+            <p>募集要項</p>      
+          </div>
+          <div class="t-recruitContent__list">
+            <dl>
+              <dt>業種</dt>
+              <dd>□□□□■□□□□■</dd>
+            </dl>    
+            <dl>
+              <dt>業務内容</dt>
+              <dd>・□□□□■□□□□■<br />
+                  ・□□□□■□□□□■<br />
+                  ・□□□□■□□□□■
+              </dd>
+            </dl>                              
+            <dl>
+              <dt>勤務形態</dt>
+              <dd>正社員（試用期間6か月）</dd>
+            </dl>
+            <dl>
+              <dt>勤務地</dt>
+              <dd>〒103-0014<br />
+                  東京都中央区日本橋蛎殻町2-11-2<br />
+                  オートエックス工藤ビル6階</dd>
+            </dl>                      
+            <dl>
+              <dt>勤務時間</dt>
+              <dd>月曜日～金曜日　土曜日・日曜日祝日休み<br />
+                  9:00~18:00<br />
+                  有給休暇あり<br />
+                  夏季・冬季休暇あり</dd>
+            </dl>                     
+            <dl>
+              <dt>給与</dt>
+              <dd>要相談</dd>
+            </dl>                      
+            <dl>
+              <dt>応募方法</dt>
+              <dd>履歴書（写真貼付）、職務経歴書をご郵送またはメールで受け付けております。<br />
+                  書類選考の上、面接日をご連絡致します。</dd>
+            </dl>                                         
+          </div>
+        </div>
+
+        <div class="p-recruitBtn">
+          <a href="https://strada-tax.jp/recruit/form">
+              <img src="<?php echo get_template_directory_uri(); ?>/images/recruit/recruit_icon.png" alt="" />
+              採用エントリーはこちらから
+          </a>
+        </div>      
+      </div>
+    </div>
+  </div>
+</main>
