27fdbbb0f6eb6b731a67316628d2f877506259c7
lynlyntan <lyntan@outlook.jp>
2023-07-28 20:36:26 +0900

    price

---
+++ wp-content/themes/strada_theme/template-price_ss.php
@@ -0,0 +1,359 @@
+<div class="t-price">
+  <div class="c-mainTitle">
+    <div class="c-mainTitle_content">
+      <div class="c-tlt01 c-tlt01__black">
+        <h1 class="c-tlt01__line c-tlt01__line--gray">
+        料金表
+          <span>Price</span>
+        </h1>
+      </div>
+    </div>
+  </div>
+  <div class="l-contents__inner l-inner mt50">
+      <div class="l-primary">
+          <div class="p-entry__body body1">
+              <div class="price_head">「顧問契約」の料金</div>
+              <h3 class="left_heading">アドバイザリー顧問</h3>
+              <figure class="wp-block-table is-style-stripes">
+                  <table>
+                          <tr>
+                              <th class="has-text-align-center">人数</th>
+                              <td class="has-text-align-center">～4人</td>
+                              <td class="has-text-align-center">5～9人</td>
+                              <td class="has-text-align-center">10～19人</td>
+                              <td class="has-text-align-center">20～29人</td>
+                              <td class="has-text-align-center">30～39人</td>
+                              <td class="has-text-align-center">40～49人</td>
+                              <td class="has-text-align-center">50人～</td>
+                          </tr>
+                      
+                          <tr>
+                              <th class="has-text-align-center">金額（月額）</th>
+                              <td class="has-text-align-center">10,000円</td>
+                              <td class="has-text-align-center">10,000円</td>
+                              <td class="has-text-align-center">10,000円</td>
+                              <td class="has-text-align-center">10,000円</td>
+                              <td class="has-text-align-center">10,000円</td>
+                              <td class="has-text-align-center">10,000円</td>
+                              <td class="has-text-align-center">応相談</td>
+                          </tr>
+                  </table>
+              </figure>
+              <div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+              <h3 class="left_heading">給与計算顧問</h3>
+              <figure class="wp-block-table is-style-stripes">
+                  <table>
+                          <tr>
+                              <th class="has-text-align-center">基本料金月額</th>
+                              <td class="has-text-align-center">10,000円+1,000円×人数</td>
+                          </tr>
+                  </table>
+              </figure>
+              <div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+              <h3 class="left_heading">労務・手続顧問</h3>
+              <figure class="wp-block-table is-style-stripes">
+                  <table>
+                          <tr>
+                              <th class="has-text-align-center">人数</th>
+                              <td class="has-text-align-center">～4人</td>
+                              <td class="has-text-align-center">5～9人</td>
+                              <td class="has-text-align-center">10～19人</td>
+                              <td class="has-text-align-center">20～29人</td>
+                              <td class="has-text-align-center">30～39人</td>
+                              <td class="has-text-align-center">40～49人</td>
+                              <td class="has-text-align-center">50人～</td>
+                          </tr>
+                          <tr>
+                              <th class="has-text-align-center">金額（月額）</th>
+                              <td class="has-text-align-center">20,000円</td>
+                              <td class="has-text-align-center">25,000円</td>
+                              <td class="has-text-align-center">40,000円</td>
+                              <td class="has-text-align-center">50,000円</td>
+                              <td class="has-text-align-center">60,000円</td>
+                              <td class="has-text-align-center">70,000円</td>
+                              <td class="has-text-align-center">応相談</td>
+                          </tr>
+                  </table>
+              </figure>
+          </div>
+          <div  class="p-entry__body body2">
+          <div class="price_head">「スポット契約」の料金</div>
+              <h3 class="left_heading">給与計算顧問</h3>
+              <figure class="wp-block-table is-style-stripes">
+                  <table>
+                      
+                          <tr>
+                              <th class="has-text-align-left">助成金診断・相談</th>
+                              <td class="has-text-align-left">0円</td>
+                          </tr>
+                          <tr>
+                               <th class="has-text-align-left">採用関係助成金申請資料作成・提出代行</th>
+                               <td class="has-text-align-left">報酬20%</td>
+                          </tr>
+                          <tr>
+                               <th class="has-text-align-left">求人票作成</th>
+                               <td class="has-text-align-left">50,000円～</td>
+                          </tr>
+                          <tr>
+                              <th class="has-text-align-left">ハローワークへの求人の申込み</th>
+                              <td class="has-text-align-left">20,000円～</td>
+                          </tr>
+                          <tr>
+                               <th class="has-text-align-left">各求人媒体への求人広告の掲載</th>
+                               <td class="has-text-align-left">求人媒体に応じる</td>
+                          </tr>
+                          <tr>
+                              <th class="has-text-align-left">人材診断ソフト利用（CUBIC）（一人当たり）</th>
+                              <td class="has-text-align-left">2,500円～3,500円</td>
+                          </tr>
+                  </table>
+              </figure>
+              <div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+              <h3 class="left_heading">入退社時支援</h3>
+              <figure class="wp-block-table is-style-stripes">
+                <table>
+                    <tr>
+                        <th class="has-text-align-left">社会保険の資格取得手続</th>
+                        <td class="has-text-align-left">5,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">雇用保険の資格取得手続</th>
+                        <td class="has-text-align-left">5,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">被扶養者異動届・国民年金第3号被保険者届</th>
+                        <td class="has-text-align-left">5,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">健康保険被保険者証・年金手帳再交付申請</th>
+                        <td class="has-text-align-left">5,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">労働条件・就業規則説明（1回当たり）</th>
+                        <td class="has-text-align-left">30,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">雇用契約書作成</th>
+                        <td class="has-text-align-left">5,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">雇用契約書の更新・面談</th>
+                        <td class="has-text-align-left">10,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">労働者名簿作成（一人当たり</th>
+                        <td class="has-text-align-left">1,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">雇用保険被保険者離職証明書</th>
+                        <td class="has-text-align-left">10,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">雇用保険被保険者資格喪失届</th>
+                        <td class="has-text-align-left">5,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">社会保険被保険者資格喪失証明書</th>
+                        <td class="has-text-align-left">5,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">健康保険任意継続被保険者資格取得申請書</th>
+                        <td class="has-text-align-left">5,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">健康保険被保険者証滅失届・回収不能届</th>
+                        <td class="has-text-align-left">5,000円</td>
+                    </tr>
+                </table>
+              </figure>
+              <div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+              <h3 class="left_heading">中小事業主が労災保険に加入したい時</h3>
+              <figure class="wp-block-table is-style-stripes">
+                  <table>
+                          <tr>
+                              <th class="has-text-align-left">労災保険加入手続き</th>
+                              <td class="has-text-align-left">20,000円</td>
+                          </tr>
+                  </table>
+              </figure>
+              <div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+              <h3 class="left_heading">中小事業主が労災保険に加入したい時</h3>
+              <figure class="wp-block-table is-style-stripes">
+                  <table>
+                          <tr>
+                              <th class="has-text-align-left">60歳到達</th>
+                              <td class="has-text-align-left">10,000円</td>
+                          </tr>
+                          <tr>
+                            <th class="has-text-align-left">60歳到達時以降</th>
+                              <td class="has-text-align-left">20,000円</td>
+                          </tr>
+                  </table>
+              </figure>
+              <div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+              <h3 class="left_heading">出産・育児・介護休暇</h3>
+              <figure class="wp-block-table is-style-stripes">
+                  <table>
+                    <tr>
+                        
+                        <th class="has-text-align-left">妊娠出産時労働条件説明</th>
+                        <td class="has-text-align-left">5,000円</td>
+                        
+                    </tr>
+                    <tr>
+                       
+                        <th class="has-text-align-left">産前産後時における休業給付金手続き・申請代行</th>
+                        <td class="has-text-align-left">5,000円</td>
+                        
+                    </tr>
+                    <tr>
+                        
+                        <th class="has-text-align-left">育児休業給付金手続き・申請代行</th>
+                        <td class="has-text-align-left">5,000円</td>
+                        
+                    </tr>
+                    <tr>
+                       
+                        <th class="has-text-align-left">介護休業時の労働条件説明</th>
+                        <td class="has-text-align-left">5,000円</td>
+                        
+                    </tr>
+                    <tr>
+                        
+                        <th class="has-text-align-left">介護休業における給付金手続き・申請代行</th>
+                        <td class="has-text-align-left">5,000円</td>
+                        
+                    </tr>
+                  </table>
+              </figure>
+              <div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+              <h3 class="left_heading">従業員が怪我をした場合</h3>
+              <figure class="wp-block-table is-style-stripes">
+                  <table>
+                  <tr>
+                        
+                        <th class="has-text-align-left">基本就業規則の作成</th>
+                        <td class="has-text-align-left">200,000円～</td>
+                       
+                    </tr>
+                    <tr>
+                       
+                        <th class="has-text-align-left">基本就業規則の変更</th>
+                        <td class="has-text-align-left">80,000円</td>
+                        
+                    </tr>
+                    <tr>
+                        
+                        <th class="has-text-align-left">パートタイム就業規則の作成</th>
+                        <td class="has-text-align-left">50,000円</td>
+                        
+                    </tr>
+                    <tr>
+                        
+                        <th class="has-text-align-left">パートタイム就業規則の変更</th>
+                        <td class="has-text-align-left">50,000円</td>
+                       
+                    </tr>
+                    <tr>
+                        
+                        <th class="has-text-align-left">賃金規程作成</th>
+                        <td class="has-text-align-left">30,000円</td>
+                        
+                    </tr>
+                    <tr>
+                       
+                        <th class="has-text-align-left">短時間正社員制度規程作成</th>
+                        <td class="has-text-align-left">20,000円</td>
+                        
+                    </tr>
+                    <tr>
+                        
+                        <th class="has-text-align-left">育児・介護規程作成</th>
+                        <td class="has-text-align-left">20,000円</td>
+                        
+                    </tr>
+                    <tr>
+                        
+                        <th class="has-text-align-left">退職金規程作成・整備</th>
+                        <td class="has-text-align-left">10,000円</td>
+                       
+                    </tr>
+                    <tr>
+                        
+                        <th class="has-text-align-left">フレックスタイム制に関する協定書作成</th>
+                        <td class="has-text-align-left">10,000円</td>
+                        
+                    </tr>
+                    <tr>
+                        
+                        <th class="has-text-align-left">一年単位の変形労働時間制に関する協定届作成</th>
+                        <td class="has-text-align-left">10,000円</td>
+                        
+                    </tr>
+                    <tr>
+                        
+                        <th class="has-text-align-left">一箇月単位の変形労働時間制に関する協定届作成</th>
+                        <td class="has-text-align-left">10,000円</td>
+                        
+                    </tr>
+                    <tr>
+                    <th class="has-text-align-left">一週間単位の否定形変形労働時間制に関する協定届作成</th>
+                    <td class="has-text-align-left">10,000円</td>
+                    </tr>
+
+                    <tr>
+                        <th class="has-text-align-left">三六協定届作成</th>
+                        <td class="has-text-align-left">10,000円</td>
+                    </tr>
+                    <tr>
+                        <th class="has-text-align-left">事業場外労働のみなし労働時間制に関する協定届作成</th>
+                        <td class="has-text-align-left">10,000円</td>
+                           
+                    </tr>
+                    <tr>
+                         <th class="has-text-align-left">専門業務型・企画業務型裁量労働制に関する協定届作成</th>
+                            <td class="has-text-align-left">20,000円</td>
+                    </tr>
+                  </table>
+              </figure>
+              <div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+              <h3 class="left_heading">調査立会い</h3>
+              <figure class="wp-block-table is-style-stripes">
+                  <table>
+                    <tr>
+                    <th class="has-text-align-left">年金事務所調査立会い（1回当たり）</th> 
+                    <td class="has-text-align-left">20,000円</td>
+                    </tr>
+                    <tr>
+                    <th class="has-text-align-left">労働基準監督署立会い（1回当たり）</th>
+                    <td class="has-text-align-left">20,000円</td>
+                    </tr>
+                    <tr>
+                    <th class="has-text-align-left">労働基準監督署からの是正勧告に対する報告書作成</th>
+                    <td class="has-text-align-left">80,000円</td>
+                    </tr>
+                  </table>
+              </figure>
+              <div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+              <h3 class="left_heading">アウトソーシング支援</h3>
+              <figure class="wp-block-table is-style-stripes">
+                  <table>
+                      <tr>
+                        <th class="has-text-align-left">社会保険の算定届（7月）</th>
+                         <td class="has-text-align-left">15,000円～</td>
+                      </tr>
+                          <tr>
+                              <th class="has-text-align-left">労働保険料の確定申告（7月）</th>
+                              <td class="has-text-align-left">15,000円～</td>
+                          </tr>
+                          <tr>
+                            <th class="has-text-align-left">賞与計算・届出、社会保険の随時改定手続き</th>
+                              <td class="has-text-align-left">10,000円</td>
+                          </tr>
+                      
+                  </table>
+              </figure>
+          </div>
+      </div>
+  </div>
+</div>
