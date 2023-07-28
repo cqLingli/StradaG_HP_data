d9a71eb7a54f6a527ed61ed450873f250846e0dc
lynlyntan <lyntan@outlook.jp>
2023-07-28 16:54:58 +0900

    about

---
+++ wp-content/themes/strada_theme/template-about_ss.php
@@ -0,0 +1,172 @@
+<div class="c-mainTitle">
+      <div class="c-mainTitle_content">
+        <div class="c-tlt01 c-tlt01__black">
+          <h1 class="c-tlt01__line c-tlt01__line--gray">
+          法人概要
+            <span>About</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div class="p-about">                           
+        <div class="p-about_group t-block" id="法人概要" >
+          <div class="c-tlt02 c-tlt02--center">                      
+            <h2>法人概要</h2>      
+          </div>
+          <div class="p-about_groupContent">
+            <div class="company_left">
+              <div class="t-company_list">
+                <dl>
+                  <dt>法人名</dt>
+                  <dd>ストラーダ社会保険労務士法人</dd>
+                </dl>                                 
+                <dl>
+                  <dt>代表</dt>
+                  <dd>宿谷 裕樹</dd>
+                </dl>
+                <dl>
+                  <dt>登録番号</dt>
+                  <dd>全国社会保険労務士連合会<br />
+                      第1318007号、第13170127号</dd>
+                </dl>
+                <dl>
+                  <dt>本店所在地</dt>
+                  <dd>〒103-0014 東京都中央区日本橋蛎殻町2-11-2 オートエックス工藤ビル3階</dd>
+                </dl>                      
+                <dl>
+                  <dt>電話番号</dt>
+                  <dd>03-6231-1103</dd>
+                </dl>                     
+                <dl>
+                  <dt>メールアドレス</dt>
+                  <dd>strada-contact@strada-sr.jp</dd>
+                </dl>                      
+                <dl>
+                  <dt>営業時間</dt>
+                  <dd>月曜日～金曜日　9時～18時　※土曜日・日曜日・祝日休み</dd>
+                </dl> 
+                <dl>
+                  <dt>業務内容</dt>
+                  <dd>・新規設立後の労務手続き<br />
+                      ・給与計算<br />
+                      ・社会保険・労働保険<br />
+                      ・就業規則作成<br />
+                      ・助成金コンサルティング<br />
+                      ・外国人労働者受入対応<br />
+                      ・人材派遣・有料職業紹介<br />
+                      ・顧問契約<br />
+                      ・人材採用コンサルティング</dd>
+                </dl>                                         
+              </div>
+            </div>
+            <div class="company_right">
+              <div class="access google-map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.732793825305!2d139.78441761525877!3d35.68358088019382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188945bc013343%3A0x91fe51cc7df02e4d!2z5pel5pys44CB44CSMTAzLTAwMTQg5p2x5Lqs6YO95Lit5aSu5Yy65pel5pys5qmL6JuO5q6755S677yS5LiB55uu77yR77yR4oiS77ySIOOCquODvOODiOOCqOODg-OCr-OCueW3peiXpOODk-ODqyA0Zg!5e0!3m2!1sja!2s!4v1635329371742!5m2!1sja!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+              </div>
+              <div class="t-company_list">
+                <dl>
+                  <dt>最寄駅 アクセス方法</dt>
+                  <dd>
+                    半蔵門線　　水天宮前　徒歩2分
+                    <a href="#access01">
+                      経路はこちら
+                      <img src="<?php echo get_template_directory_uri(); ?>/images/about/migi_icon.png" alt="" />
+                    </a>
+                    <br />
+                    日比谷線　　人形町駅　徒歩5分
+                    <a href="#access02">
+                      経路はこちら
+                      <img src="<?php echo get_template_directory_uri(); ?>/images/about/migi_icon.png" alt="" />
+                    </a>
+                    <br />
+                    浅草線　　　人形町駅　徒歩8分
+                    <a href="#access02">
+                      経路はこちら
+                      <img src="<?php echo get_template_directory_uri(); ?>/images/about/migi_icon.png" alt="" />
+                    </a>
+                    <br />
+                    都営新宿線　浜町駅　　徒歩10分
+                    <a href="#access03">
+                      経路はこちら
+                      <img src="<?php echo get_template_directory_uri(); ?>/images/about/migi_icon.png" alt="" />
+                    </a>
+                  </dd>
+                </dl>
+              </div>
+            </div>
+          </div>
+        </div>
+                                            
+        <div class="p-about_history t-block" id="ストラーダグループの沿革">
+          <div class="c-tlt02 c-tlt02--center">                      
+            <h2>ストラーダグループの沿革</h2>      
+          </div>
+          <div class="p-about_historyContent">
+            <div class="p-about_historyTime">                                
+              <dl>
+                <dt>2021年00月</dt>
+                <dd>
+                  <span>タックスリアルティ株式会社を設立</span><br />
+                  タックスリアルティ株式会社の代表取締役に加藤敦が就任致しました。
+                </dd>
+              </dl>                     
+              <dl>
+                <dt>2021年4月</dt>
+                <dd><span>ストラーダビジネスサポート株式会社は、有料職業紹介事業許可を取得</span><hr>
+                <span>ストラーダ行政書士法人を設立</span><br />
+                    ストラーダ行政書士法人の代表社員に星野隆介が就任致しました。</dd>
+              </dl>                     
+              <dl>
+                <dt>2019年10月</dt>
+                <dd>ストラーダビジネスサポート株式会社の取締役を吉永誠が辞任致しました。</dd>
+              </dl>                     
+              <dl>
+                <dt>2018年11月</dt>
+                <dd><span>塚田拓也が社員として就任</span><br />
+                    ストラーダ税理士法人の社員に公認会計士税理士の塚田拓也が就任致しました。
+                </dd>
+              </dl>                     
+              <dl>
+                <dt>2018年4月</dt>
+                <dd><span>労働保険事務組合 中小企業労働企画開発協会を設立</span><br />
+                    労働保険事務組合 中小企業労働企画開発協会の会長に宿谷裕樹が就任致しました。</dd>
+              </dl> 
+              <dl>
+                <dt>2018年1月</dt>
+                <dd>ストラーダビジネスサポート株式会社の取締役に塚田拓也と宿谷祐樹が就任致しました。</dd>
+              </dl>
+              <dl>
+                <dt>2017年11月</dt>
+                <dd><span>ストラーダ社会保険労務士法人として法人化</span><br />
+                    ストラーダ社会保険労務士法人の代表社員に宿谷裕樹が就任致しました。</dd>
+              </dl>
+              <dl>
+                <dt>2017年3月</dt>
+                <dd><span>ストラーダ社会保険労務士事務所を設立</span></dd>
+              </dl>
+              <dl>
+                <dt>2016年7月</dt>
+                <dd>ストラーダビジネスサポート株式会社の代表取締役に山田直輝が、取締役に吉永誠が就任致しました。</dd>
+              </dl>
+              <dl>
+                <dt>2016年6月</dt>
+                <dd>
+                  <span>ストラーダビジネスサポート株式会社を設立</span>
+                  <hr>
+                  <span>ストラーダ税理士法人は、経済産業省より経営革新認定支援機関として認定</span><br />
+                    ストラーダ税理士法人は、中小企業の新たな事業活動の促進に関する法律に基づき、２０１６０６０３関東第４２号及び関財金１第46号により、平成28年6月17日に経済産
+                    業大臣より、経営革新認定支援機関として認定されました。</dd>
+              </dl>                   
+              <dl>
+                <dt>2015年12月</dt>
+                <dd><span>山田直輝税理士事務所を法人化してストラーダ税理士法人を設立</span><br />
+                2015年12月23日に、山田直輝税理士事務所と佐藤廣子税理士事務所を統合して、法人化に伴い、東京都中央区日本橋でストラーダ税理士法人を設立致しました。</dd>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
