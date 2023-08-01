b27af67fd27680c975d627690c4d333738ac03c7
shiguiyong <shiguiyong7539@outlook.com>
2023-08-01 19:13:23 +0800

    sz_service 改修

--- wp-content/themes/strada_theme/archive-sz-service.php
+++ wp-content/themes/strada_theme/archive-sz-service.php
@@ -21,7 +21,15 @@ get_header("sz");
       </div>
     </div>
   </div>
-    <hr class="service-line-bottom" />
+
+    <div style="padding: 1% 2%;background: white">
+        <hr class="service-line-bottom2" />
+    <p style="margin-top: 10px;">私たちは、企業が誕生して、軌道に乗り、成長していく姿を数多く見てきました。</p>
+    <p>その中で、資金調達、補助金の取得、資本政策に基づく、会社のM&Aや上場等の企業の大きなライフイベントが発生します。</p>
+    <p>そんな、会社のライフイベントで頼りになる存在であり続けたいと考えております。</p>
+        <hr class="service-line-bottom2" />
+    </div>
+
   <!-- end c-mainTitle -->
     <div class="c-new-content">
   <div class="l-content">
@@ -35,28 +43,41 @@ get_header("sz");
           ) );
       ?>
       <div class="c-serviceList">
+          <div class="menberTtile">事業内容</div>
+          <div style="height: 10px;"></div>
         <?php 
-        
+           $index=0;
         foreach ( $terms as $term ) :
         $tax_link = get_term_link($term->slug, 'taxonomy_service');
         $tax_name = $term->name;
         $tax_slug = $term->slug;
         $img = get_field('service_cat_thumbnail', $term);
+        $index++;
         ?>
-          <div class="serviceItem">
-              <span class="ttl"><?php echo $tax_name; ?></span>
+          <div class="<?php
+          if($index%3==1){
+             echo "serviceItem_top";
+          }else{
+              echo "serviceItem_top2";
+          }
+          ?>">
             <div class="thumb">
               <img src="<?php echo $img['url'] ?>" alt="">
             </div>
-
+              <div class="service_title_icon">
+              <a  href="<?php echo get_term_link($term) ?>">
+              <span><?php echo $tax_name; ?></span>
+              <img src="<?php echo get_template_directory_uri(); ?>/images/top/news_more.png">
+              </a>
+              </div>
             <div class="content">
                 <div>
-              <p><?php echo nl2br($term->description); ?></p>
+              <p class="description_box"><?php echo nl2br($term->description); ?></p>
 <!--              <div class="c-btn01 ">-->
                 </div>
-               <div class="ttl2 c-btn01__blue">
-                <a href="<?php echo get_term_link($term) ?>">詳細はこちら</a>
-              </div>
+<!--               <div class="ttl2 c-btn01__blue">-->
+<!--                <a href="--><?php //echo get_term_link($term) ?><!--">詳細はこちら</a>-->
+<!--              </div>-->
             </div>
 
           </div>
