5d24e1ff5a6a59434093b646f50f75962b97a534
Lmouwd <731579290@qq.com>
2023-08-03 19:08:56 +0800

    top_sz アンカーリンク修正

--- wp-content/themes/strada_theme/front-page_SZ.php
+++ wp-content/themes/strada_theme/front-page_SZ.php
@@ -259,7 +259,8 @@
   <!-- end Message -->
 
   <!-- start Strada Group -->
-  <div id="StradaGroup" class="block_wrap2">
+  <a id="StradaGroup" class="target-fix"></a>
+  <div class="block_wrap2">
     <div class="border-text2"></div>
     <div class="l-container">
       <div id="block_wrap2" class="title_item_wrap2 active2">
@@ -298,7 +299,8 @@
   <!-- end Strada Group -->
 
   <!-- start Service -->
-  <div id="Service" class="block_wrap3">
+  <a id="Service" class="target-fix"></a>
+  <div class="block_wrap3">
     <div class="border-text3"></div>
     <div class="l-container">
       <div id="block_wrap3" class="title_item_wrap3 active3">
@@ -311,12 +313,12 @@
       <div id="list_wrap3" class="node_wrap3 node_wrap_show3">
         <?php get_template_part('template', 'sz-service'); ?>
       </div>
-
   </div>
   <!-- end Service -->
 
   <!-- start Voice -->
-  <div id="Voice" class="block_wrap4">
+  <a id="Voice" class="target-fix"></a>
+  <div class="block_wrap4">
     <div class="border-text4"></div>
     <div class="l-container">
       <div id="block_wrap4" class="title_item_wrap4 active4">
@@ -333,7 +335,8 @@
   <!-- end Voice -->
 
   <!-- start Q&A -->
-  <div id="QA" class="block_wrap5">
+  <a id="QA" class="target-fix"></a>
+  <div class="block_wrap5">
     <div class="border-text5"></div>
     <div class="l-container">
       <div id="block_wrap5" class="title_item_wrap5 active5">
@@ -350,7 +353,8 @@
   <!-- end Q&A -->
 
   <!-- start Column -->
-  <div id="Column" class="block_wrap6">
+  <a id="Column" class="target-fix"></a>
+  <div class="block_wrap6">
     <div class="border-text6"></div>
     <div class="l-container">
       <div id="block_wrap6" class="title_item_wrap6 active6">
@@ -368,7 +372,8 @@
   <!-- end Column -->
 
   <!-- start About -->
-  <div id="About" class="block_wrap7">
+  <a id="About" class="target-fix"></a>
+  <div class="block_wrap7">
     <div class="border-text7"></div>
     <div class="l-container">
       <div id="block_wrap7" class="title_item_wrap7 active7">
@@ -380,7 +385,7 @@
     <div class="border-bt7"></div>
       <div id="list_wrap7" class="node_wrap7 node_wrap_show7">
         <?php get_template_part('template', 'about_sz'); ?>
-        <div id="menber"></div>
+        <a id="menber" class="target-fix1"></a>
         <?php get_template_part('template', 'sz-members'); ?>
       </div>
   </div>
@@ -406,7 +411,8 @@
   <!-- end Contact -->
 
   <!-- start Recruit -->
-  <div id="Recruit" class="block_wrap9">
+  <a id="Recruit" class="target-fix"></a>
+  <div class="block_wrap9">
     <div class="border-text9"></div>
     <div class="l-container">
       <div id="block_wrap9" class="title_item_wrap9 active9">
