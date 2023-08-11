e76a151d22a2708671a5e54b538fde34c311a41f
shiguiyong <shiguiyong7539@outlook.com>
2023-08-11 09:47:23 +0800

    top-menber,news

--- wp-content/themes/strada_theme/archive-members.php
+++ wp-content/themes/strada_theme/archive-members.php
@@ -6,207 +6,118 @@ $_SESSION['url_type']="";
 $_SESSION['page_type']="members";
 ?>
 <!-- .l-main | メインコンテンツ -->
-<main class="l-main p-member01">
-  <div class="c-breakcrumds">
-    <div class="l-container">
-      <?php if (function_exists('bcn_display')) { bcn_display(); }?>
-    </div>
-  </div>
-  <!-- end c-breakcrumds -->
-  <div class="c-mainTitle">
-    <div class="c-mainTitle_content">
-      <div class="c-tlt01 c-tlt01__black">
-        <h1 class="c-tlt01__line c-tlt01__line--gray">
-          メンバー紹介
-          <span>MEMBER</span>
-        </h1>
-      </div>
-    </div>
-  </div>
-  <!-- end c-mainTitle -->
-  <div class="l-container">
-    <ul class="p-member01__link" id="myBtnContainer">
-      <li class="is-active"><a href="<?php echo get_post_type_archive_link('members'); ?>">ALL</a></li>
-      <?php
-          $terms = get_terms( 'taxonomy_position', array(
-            'hide_empty' => false,
-            'parent' => 0,
-            'order' => 'ASC',
-            'orderby' => 'term_id'
-          ) );
-          foreach ( $terms as $term ) {
-            $tax_link = get_term_link($term->slug, 'taxonomy_position');
-            $tax_name = $term->name;
-            $tax_slug = $term->slug;
-            echo '<li><a href="'.get_term_link($term).'">'.$tax_name.'</a></li>';
-          }
-      ?>
-    </ul>
-    <div class="p-member__list">
-      <?php while ( have_posts() ) : the_post(); ?>
-        <div class="item">
-          <div class="image">
-            <?php
-              if ( has_post_thumbnail() ) {
-                the_post_thumbnail('full');
-              } else {
-            ?>
-              <img src="<?php echo get_template_directory_uri(); ?>/images/noimage.png" alt="<?php the_title(); ?>">
-            <?php } ?>
-          </div>
-          <div class="txt">
-            <div class="c-job">
-              <?php
-                $terms = wp_get_post_terms($post->ID,'taxonomy_position', array(
-                    'hide_empty' => false,
-                    'parent' => 0,
-                    'order' => 'ASC',
-                    'orderby' => 'term_id'
-                ) );
-                foreach ( $terms as $term) {
-                  $tax_link = get_term_link($term->slug, 'taxonomy_position');
-                  $tax_name = $term->name;
-                  $tax_slug = $term->slug;
-                  echo '<p class="job">' . $tax_name . '</p>';
-                }
-              ?>
+    <main class="l-main p-member02">
+        <div class="c-breakcrumds">
+            <div class="l-container">
+                <?php if (function_exists('bcn_display')) { bcn_display(); }?>
             </div>
-            <p class="name"><?php echo get_the_title(); ?></p>
-          </div>
-          <a href="<?php echo get_permalink(get_the_ID()); ?>"></a>
         </div>
-      <?php endwhile; ?>
-      <!-- <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img02.jpg" alt="" />
+        <!-- end c-breakcrumds -->
+        <div class="c-mainTitle">
+            <div class="c-mainTitle_content menber-content">
+                <div class="c-tlt01 c-tlt01__black">
+                    <h1 class="c-tlt01__line c-tlt01__line--gray">
+                        メンバー紹介
+                        <span>MEMBER</span>
+                    </h1>
+                </div>
+            </div>
         </div>
-        <div class="txt">
-          <p class="job">税理士</p>
-          <p class="name">塚田 拓也</p>
+        <!-- end c-mainTitle -->
+        <div class="c-new-content" style="padding-bottom: 100px;">
+            <div class="l-container">
+                <div class="p-member__list">
+                    <div class="menberTtile3">
+                        <span>メンバー紹介</span>
+                    </div>
+                    <?php
+                    $index_menber=0;
+                    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : '1';
+                    $args = array(
+                        'post_type' => 'members',
+                        'posts_per_page' => -1,
+                        'orderby' => 'date',
+                        'paged' => $paged
+                    );
+
+                    $the_query = new WP_Query($args); ?>
+                    <?php
+                    if($the_query->have_posts()) :
+                        while($the_query->have_posts()) : $the_query->the_post();  $index_menber++;?>
+                            <div class="<?php
+                            if($index_menber % 4==1){
+                                echo "newItem";
+                            }else{
+                                echo "newItem2";
+                            }
+                            ?>">
+                                <div class="image">
+                                    <?php
+                                    if ( has_post_thumbnail() ) {
+                                        the_post_thumbnail('full');
+                                    } else {
+                                        ?>
+                                        <img src="<?php echo get_template_directory_uri(); ?>/images/noimage.png" alt="<?php the_title(); ?>">
+                                    <?php } ?>
+                                </div>
+                                <div class="txt">
+                                    <?php
+                                    $representative_name="";
+                                    $tax_name_over="";
+                                    $terms = wp_get_post_terms($post->ID,'taxonomy_position', array(
+                                        'hide_empty' => false,
+                                        'parent' => 0,
+                                        'order' => 'ASC',
+                                        'orderby' => 'term_id'
+                                    ) );
+                                    $index=0;
+                                    foreach ( $terms as $term) {
+                                        $tax_link = get_term_link($term->slug, 'taxonomy_position');
+                                        $tax_name = $term->name;
+                                        $tax_slug = $term->slug;
+                                        if($tax_name==="代表社員"){
+                                            $representative_name  ="代表社員";
+                                        }else{
+                                            if($index===0){
+                                                //  echo '<p class="job">'.$tax_name.'</p>';
+                                                $tax_name_over=$tax_name;
+                                            }else{
+                                                //   echo '<p class="job">/'.$tax_name.'</p>';
+                                                $tax_name_over=$tax_name_over.'/'.$tax_name;
+                                            }
+                                            $index++;
+                                        }
+
+                                    }
+                                    ?>
+                                    <p class="menber-representative" style="height: 20px;"><?php echo $representative_name; ?></p>
+                                    <p class="menber-name"><?php echo get_the_title(); ?></p>
+                                    <div class="c-job">
+                                        <p class="job"><?php echo $tax_name_over ?></p>
+                                    </div>
+
+                                    <a  href="<?php echo get_permalink(get_the_ID());?>">
+                                        <div class="service_title_icon">
+                                            <span style="font-size: 1.5rem;font-weight: 400;">READ MORE</span>
+                                        </div>
+                                    </a>
+
+                                </div>
+                            </div>
+
+                        <?php endwhile;
+                        if (function_exists("pagination")) {
+                            pagination($the_query->max_num_pages);
+                        }
+                        wp_reset_postdata();
+                    else:
+                        ?>
+                        <div class="title"><p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p></div>
+                    <?php
+                    endif;
+                    ?>
+                </div>
+            </div>
         </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img03.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">税理士</p>
-          <p class="name">星野 隆介</p>
-        </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img04.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">役職名入ります</p>
-          <p class="name">宿谷</p>
-        </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img05.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">役職名入ります</p>
-          <p class="name">吉永</p>
-        </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img06.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">役職名入ります</p>
-          <p class="name">鈴木</p>
-        </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img07.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">役職名入ります</p>
-          <p class="name">宮口</p>
-        </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img08.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">役職名入ります</p>
-          <p class="name">伊藤</p>
-        </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img09.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">役職名入ります</p>
-          <p class="name">奥崎</p>
-        </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img10.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">役職名入ります</p>
-          <p class="name">山下</p>
-        </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img11.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">役職名入ります</p>
-          <p class="name">古野</p>
-        </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img12.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">役職名入ります</p>
-          <p class="name">片岡</p>
-        </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img13.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">役職名入ります</p>
-          <p class="name">三宅</p>
-        </div>
-        <a href="#"></a>
-      </div>
-      <div class="item">
-        <div class="image">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/member/img14.jpg" alt="" />
-        </div>
-        <div class="txt">
-          <p class="job">役職名入ります</p>
-          <p class="name">小川</p>
-        </div>
-        <a href="#"></a>
-      </div> -->
-    </div>
-  </div>
-</main>
+    </main>
 <?php get_footer(); ?>
--- wp-content/themes/strada_theme/archive-news.php
+++ wp-content/themes/strada_theme/archive-news.php
@@ -6,44 +6,64 @@ $_SESSION['url_type']="";
 $_SESSION['page_type']="news";
 ?>
 
-<!-- .l-main | メインコンテンツ -->
-<main class="l-main p-new01">
-  <div class="c-breakcrumds">
-    <div class="l-container">
-      <?php if (function_exists('bcn_display')) { bcn_display(); }?>
-    </div>
-  </div>
-  <div class="c-mainTitle">
-    <div class="c-mainTitle_content group_content">
-      <div class="c-tlt01 c-tlt01__black">
-        <h1 class="c-tlt01__line c-tlt01__line--gray">
-          ニュース
-          <span>NEWS</span>
-        </h1>
-      </div>
-    </div>
-  </div>
-  <div class="c-new-content">
-    <div class="l-container">
-      <div class="c-list-news">
-        <?php while ( have_posts() ) : the_post(); ?>
-          <a href="<?php echo get_permalink(get_the_ID()); ?>" class="c-new">
-            <div class="c-new__date">
-              <?php echo get_the_date('Y.m.d'); ?>
+    <!-- .l-main | メインコンテンツ -->
+    <main class="l-main p-new01">
+        <div class="c-breakcrumds">
+            <div class="l-container">
+                <?php if (function_exists('bcn_display')) { bcn_display(); }?>
             </div>
-            <div class="c-new__title">
-              <?php echo get_the_title(); ?>
-            </div>
-            <hr class="c-line-bottom" />
-          </a>
-        <?php endwhile; ?>
-        <div class="c-pagination">
-          <?php wp_pagenavi(); ?>
         </div>
-      </div>
-    </div>
-  </div>
-</main>
+        <div class="c-mainTitle">
+            <div class="c-mainTitle_content news_content_add">
+                <div class="c-tlt01 c-tlt01__black">
+                    <h1 class="c-tlt01__line c-tlt01__line--gray">
+                        ニュース
+                        <span>NEWS</span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+        <div class="c-new-content" style="padding-bottom: 120px;">
+            <div class="l-container">
+                <div class="c-list-news">
+                    <?php
+                    // $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : '1';
+                    $args = array(
+                        'post_type' => 'news', // 文章类型
+                        'posts_per_page' => 10,
+                        'paged' => $paged
+                    );
+                    $query = new WP_Query( $args ); // 执行查询
+                    ?>
+                    <?php while (  $query->have_posts() ) : $query->the_post(); ?>
+                        <a href="<?php echo get_permalink(get_the_ID()); ?>" class="c-new">
+                            <div class="c-new__date">
+                                <?php echo get_the_date('Y.m.d'); ?>
+                            </div>
+                            <div class="c-new__title">
+                                <?php echo get_the_title(); ?>
+                            </div>
+                            <hr class="c-line-bottom" />
+                        </a>
+                    <?php endwhile; ?>
+
+                    <!--          --><?php //if(function_exists('wp_pagenavi')) { ?>
+                    <!--              --><?php //wp_pagenavi(); ?>
+                    <!--          --><?php //} ?>
+
+                </div>
+                <?php
+                if (function_exists("pagination")) {
+
+                    pagination($query->max_num_pages);
+                }
+                ?>
+                <!--        <div class="c-pagination">-->
+                <!--            --><?php // wp_pagenavi(); ?>
+                <!--        </div>-->
+            </div>
+        </div>
+    </main>
 
 
 <?php get_footer(); ?>
