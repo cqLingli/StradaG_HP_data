89c071d35c10928db0a91a9a18cbb07c353bcab6
Lmouwd <731579290@qq.com>
2023-08-14 14:48:16 +0800

    style_ss 初版copy作成

--- wp-content/themes/strada_theme/front-page_ss.php
+++ wp-content/themes/strada_theme/front-page_ss.php
@@ -214,7 +214,7 @@ $_SESSION['page_type']="top";
     </div>
     </div>
       <div id="list_wrap3" class="node_wrap3 node_wrap_show3">
-        <?php get_template_part('template', 'ss-service'); ?>
+        <?php get_template_part('template', ''); ?>
       </div>
   </div>
   <!-- end Service -->
