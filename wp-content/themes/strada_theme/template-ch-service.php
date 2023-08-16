830eef53daa75d02650fc3d572565fe0d4b1c2bb
Lmouwd <731579290@qq.com>
2023-08-16 14:14:49 +0800

    中小企業労働企画開発協会の業務内容 service-ch 作成

--- wp-content/themes/strada_theme/template-ch-service.php
+++ wp-content/themes/strada_theme/template-ch-service.php
@@ -13,40 +13,129 @@
             </div>
         </div>
         <div class="l-container">
-        <div class="template-service-title">
-            <span class="service-line-bottom2-span">労働保険とは？</span>
-            <hr class="service-line-bottom-top" />
-            <p>労災保険と雇用保険とを総称したものを労働保険といいます。 労災保険と雇用保険の給付は両保険制度で別に行われていますが、保険料の徴収は労働保険として一体のものとして扱われてい
-                ます。労働保険は、原則として、労働者を１人でも雇用していれば、加入手続きをおこない労働保険料を納付する必要があります。労働保険事務組合に加入することで、労働保険の特別加入の手
-                続き、労働保険の事務手続きを労働保険組合を通じておこなうことができます。</p>
-            <hr class="service-line-bottom-top2" />
+            <div class="template-service-title">
+                <span class="service-line-bottom2-span">労働保険とは？</span>
+                <hr class="service-line-bottom-top" />
+                <p>労災保険と雇用保険とを総称したものを労働保険といいます。 労災保険と雇用保険の給付は両保険制度で別に行われていますが、保険料の徴収は労働保険として一体のものとして扱われてい
+                    ます。労働保険は、原則として、労働者を１人でも雇用していれば、加入手続きをおこない労働保険料を納付する必要があります。労働保険事務組合に加入することで、労働保険の特別加入の手
+                    続き、労働保険の事務手続きを労働保険組合を通じておこなうことができます。</p>
+                <hr class="service-line-bottom-top2" />
 
-            <span class="service-line-bottom2-span">労働保険組合とは？</span>
-            <hr class="service-line-bottom-top" />
-            <p>労働保険事務は、中小企業にとって手続きがわずらわしく、負担となっている場合が少なくありません。「労働保険事務組合」として認可された事業主の団体（主に社会保険労務士法人等）が事業
-                主等の委託を受けて、労働保険料及び一般拠出金の申告・納付や労働保険の各種届出等をおこないます。</p>
-            <hr class="service-line-bottom-top2" />
-            <div style="margin-top: 3%;"></div>
-        </div>
+                <span class="service-line-bottom2-span">労働保険組合とは？</span>
+                <hr class="service-line-bottom-top" />
+                <p>労働保険事務は、中小企業にとって手続きがわずらわしく、負担となっている場合が少なくありません。「労働保険事務組合」として認可された事業主の団体（主に社会保険労務士法人等）が事業
+                    主等の委託を受けて、労働保険料及び一般拠出金の申告・納付や労働保険の各種届出等をおこないます。</p>
+                <hr class="service-line-bottom-top2" />
+                <div style="margin-top: 3%;"></div>
+            </div>
         </div>
     </div>
     <!-- end c-mainTitle -->
 
-    <div class="c-new-content-service">
+    <!--start 中小企業労働企画開発協会の業務内容 -->
+    <div class="ch_service_bg">
         <div class="l-container">
-
-            <div class="sr_service_bg">
-                <div class="l-container">
-                    <div class="sr_service_style">
-                        <p class="sr_service_gm">中小企業労働企画開発協会の業務内容</p>
-
+            <div class="ch_service_style">
+                <p class="ch_service_gm">中小企業労働企画開発協会の業務内容</p>
+                <div class="ch_service_gm_content">
+                    <p class="ch_service_gm_content_title1"><span
+                            class="ch_service_gm_content_nayo1">労災の特別加入</span><span
+                            class="ch_service_gm_content_nayo2">一定要件の下、中小事業主も労災保険に加入できます！</span></p>
+                    <div class="ch_service_gm_content_title2">
+                        <div class="ch_service_gm_content_num">1
                         </div>
+                        <div class="ch_service_gm_content_modai">特別加入するためには？</div>
                     </div>
+                    <p class="ch_service_gm_content_nayo5">・中小事業であること</p>
+                    <p class="ch_service_gm_content_title3"><span class="ch_service_gm_content_nayo3">業種</span><span
+                            class="ch_service_gm_content_nayo4">労働者数</span></p>
+                    <p class="ch_service_gm_content_title3"><span
+                            class="ch_service_gm_content_nayo3">金融業・保険業・不動産業・小売業</span><span
+                            class="ch_service_gm_content_nayo4">50人以下</span></p>
+                    <p class="ch_service_gm_content_title3"><span
+                            class="ch_service_gm_content_nayo3">卸売業・サービス業</span><span
+                            class="ch_service_gm_content_nayo4">100人以下</span></p>
+                    <p class="ch_service_gm_content_title4"><span
+                            class="ch_service_gm_content_nayo3">上記以外の業種</span><span
+                            class="ch_service_gm_content_nayo4">300人以下</span></p>
+                    <p class="ch_service_gm_content_nayo5">
+                        ・労働保険の事務処理を労働保険事務組合に委託することが必須<br />　弊社は労働保険事務組合を併設しているため、スムースに委託処理が可能です<br />・雇用する労働者がいること</p>
+
+                    <div class="ch_service_gm_content_title2">
+                        <div class="ch_service_gm_content_num">2
+                        </div>
+                        <div class="ch_service_gm_content_modai">補償の範囲は？</div>
+                    </div>
+                    <p class="ch_service_gm_content_nayo5">
+                        ・一定の制限がありますが、原則的に通常の労災と同じです<br />　業務災害も通勤災害も対象となります<br />　代表的な給付内容は下記の通りとなります</p>
+
+                    <p class="ch_service_gm_content_title3"><span
+                            class="ch_service_gm_content_nayo3">保険給付の種類</span><span
+                            class="ch_service_gm_content_nayo4">支給事由</span></p>
+                    <p class="ch_service_gm_content_title3"><span
+                            class="ch_service_gm_content_nayo3">療養（補償）給付</span><span
+                            class="ch_service_gm_content_nayo4">業務災害により病院等で治療</span></p>
+                    <p class="ch_service_gm_content_title3"><span
+                            class="ch_service_gm_content_nayo3">休業（補償）給付</span><span
+                            class="ch_service_gm_content_nayo4">業務際により4日以上勤務不能となった場合</span></p>
+                    <p class="ch_service_gm_content_title3"><span
+                            class="ch_service_gm_content_nayo3">障害（補償）給付</span><span
+                            class="ch_service_gm_content_nayo4">業務災害により障害が残った場合<br />障害等級第1級～第7級は年金<br />障害等級第8級～第14級は一時金</span>
+                    </p>
+                    <p class="ch_service_gm_content_title4"><span class="ch_service_gm_content_nayo3">遺族補償給付</span><span
+                            class="ch_service_gm_content_nayo4">業務災害により死亡した場合<br />遺族年金の受給資格をもつ遺族がいない場合は遺族一時金</span></p>
+
+                    <p class="ch_service_gm_content_title5"><span
+                            class="ch_service_gm_content_nayo1">労災の特別加入</span><span
+                            class="ch_service_gm_content_nayo2">保険料や保険給付の金額は？</span></p>
+
+                    <div class="ch_service_gm_content_title2">
+                        <div class="ch_service_gm_content_num">3
+                        </div>
+                        <div class="ch_service_gm_content_modai">保険料</div>
+                    </div>
+
+                    <p class="ch_service_gm_content_nayo5">
+                        ・ご自身で給付基礎日額（3,500円～25,000円まで）を決めて、その金額に応じて保険料が算定されます</p>
+
+                    <p class="ch_service_gm_content_nayo6">
+                        例）給付基礎日額25,000円<br />業種：卸売業→労災の保険料率3/1000<br />25,000円×365日×3/1000＝27,375（年間の労災保険料）</p>
+
+                    <p class="ch_service_gm_content_nayo5">
+                        ・この他、労働保険事務組合への入会金10,000円及び月会費5,000円～が掛かります。</p>
+
+                    <div class="ch_service_gm_content_title2">
+                        <div class="ch_service_gm_content_num">4
+                        </div>
+                        <div class="ch_service_gm_content_modai">給付の金額</div>
+                    </div>
+                    <p class="ch_service_gm_content_nayo5">
+                        ・上記で定めた保険料に応じて保険金額が算定されます<br />例）給付基礎日額25,000円の場合</p>
+
+                    <p class="ch_service_gm_content_title3"><span
+                            class="ch_service_gm_content_nayo3">保険給付の種類</span><span
+                            class="ch_service_gm_content_nayo4">給付内容・金額</span></p>
+                    <p class="ch_service_gm_content_title3"><span
+                            class="ch_service_gm_content_nayo3">療養（補償）給付</span><span
+                            class="ch_service_gm_content_nayo4">診療費無料</span></p>
+                    <p class="ch_service_gm_content_title3"><span
+                            class="ch_service_gm_content_nayo3">休業（補償）給付</span><span
+                            class="ch_service_gm_content_nayo4">30日間欠勤となった場合<br />25,000円×（60％+20％）×（30日-3日）＝540,000円</span>
+                    </p>
+                    <p class="ch_service_gm_content_title3"><span
+                            class="ch_service_gm_content_nayo3">障害（補償）給付</span><span
+                            class="ch_service_gm_content_nayo4">障害等級第1級に該当した場合<br />25,000円×313日＝7,825,000円の年金支給<br />&3,420,000円の一時金</span>
+                    </p>
+                    <p class="ch_service_gm_content_title4"><span class="ch_service_gm_content_nayo3">遺族補償給付</span><span
+                            class="ch_service_gm_content_nayo4">遺族が妻一人の場合<br />25,000円×153日＝3,825,000円の年金支給<br />&3,000,000円の一時金</span>
+                    </p>
+
+
+                    <p class="ch_service_gm_content_nayo5">
+                        ・この他、労働保険事務組合への入会金10,000円及び月会費5,000円～が掛かります。</p>
                 </div>
             </div>
         </div>
-
-
-
-
+    </div>
+    <!--end 中小企業労働企画開発協会の業務内容 -->
 </main>
